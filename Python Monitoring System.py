9fe851cd49078a916e2a1aaab8eab9adb1d81e3f
Kromods <83139914+Kromods@users.noreply.github.com>
2024-10-17 10:04:03 +0200

    CPU Warnung eingefügt bei kritischer Last

--- Python Monitoring System.py
+++ Python Monitoring System.py
@@ -1,14 +1,27 @@
 import psutil
 import time
 
+#Schwellenwert für CPU-Auslastung (hier 50 Prozent)
+CRITICAL_CPU_THRESHOLD = 50.0 
+
 #Funktion zum Überwachen der CPU-Auslastung
 def monitor_cpu():
     cpu_info = []
+    cpu_usage = psutil.cpu_percent(interval=1)
+
     cpu_info.append("CPU-Auslastung:")
     cpu_info.append(f" Gesamte CPU-Auslastung: {psutil.cpu_percent(interval=1)}%")
     cpu_info.append(f" CPU-Kerne: {psutil.cpu_count(logical=True)}")
     cpu_info.append(f" CPU-Frequenz: {psutil.cpu_freq().current} MHZ\n")
-    return "\n".join(cpu_info)
+
+    #Warnung bei zu hoher CPU Auslastung 
+    if cpu_usage >=CRITICAL_CPU_THRESHOLD:
+        warning_message = f"!!! WARNUNG: CPU-Auslastung hat {CRITICAL_CPU_THRESHOLD}% überschritten ({cpu_usage}%) !!!"
+        cpu_info.append(warning_message)
+        print(warning_message) #Gib die Warnung in der Konsole aus 
+        return "\n".join(cpu_info), warning_message #Rückgabe der CPU-Information und der Warnung
+    
+    return "\n".join(cpu_info), None #keine Warunung, nur CPU Daten zurückgeben
 
 #Funktion zum überwachen des Arbeitsspeichers
 def monitor_memory():
