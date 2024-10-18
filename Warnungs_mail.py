a24081da92d3a63c8c981454ec966f45536fd490
Kromods <83139914+Kromods@users.noreply.github.com>
2024-10-18 14:49:16 +0200

    Funktionierende Mail über MCS server

--- Warnungs_mail.py
+++ Warnungs_mail.py
@@ -1,30 +1,29 @@
 import smtplib
 from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 
 #Mail angabe sender und reciver (Empfänger Liste kann auch als Liste erstellt werden)
 def send_email_alert(subject, body):
-    sender_mail = "marten.rosinski@gmail.com" # Absender adresse hier würde ich meine Firmen Mail nehmen
+    sender_mail = "marten@schokola.de" # Absender adresse hier würde ich meine Firmen Mail nehmen
     receiver_mail = "rosinskimarten@gmail.com" # Receiver Mail ( maybe hier die Privatadressen)
-    password = "NeuealteFreunde" # Hier muss das Passwort der Absender Adresse rein (maybe App specific Passwort)
+    password = "Monitoring-cpu80" # Hier muss das Passwort der Absender Adresse rein (maybe App specific Passwort)
     
     #MIME Struktur der Mail
-    msg = MIMEMultipart()
+    msg = MIMEText(body)
     msg['From'] = sender_mail
     msg['To'] = receiver_mail
     msg['Subject'] = subject
-    
-    #Nachrichtentext
-    msg.attach(MIMEText(body, 'plain'))
 
 #Verbindung zum SMTP Server 
     try:
-        server = smtplib.SMTP('smtp.gmail.com', 465)
-        server.starttls() #Sichere VErbindung mit TLS (Transport Layer Security)
+        server = smtplib.SMTP_SSL('smtp.priv.mcs.de', 465)
+    #    server.starttls() #Sichere VErbindung mit TLS (Transport Layer Security)
+        server.connect()
+        server.ehlo()
         server.login(sender_mail, password)#Login mit Mail und Passwort
         text = msg.as_string() # Konvertiere das Email Objekt in eine Zeichenkette
-        server.sendmail(sender_mail, receiver_mail, text) # EMail senden
-        server.quit()# Verbindung zum Server trennen
+        server.send_message(msg) # EMail senden
+        server.close()# Verbindung zum Server trennen
+        server.quit()
         print("Warnung per E-Mail versendet!")
 
     except Exception as e:
